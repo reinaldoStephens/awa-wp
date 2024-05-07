54d7a23728b81541be3dccfeb125f0b0cf902ded
Reinaldo Stephens <reinaldo9519@gmail.com>
2024-05-06 19:54:37 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Main template file
+ * 
+ * @package Awa
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Awa Boots - Store</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
